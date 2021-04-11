63be261280a0add347d618982b1a4788a48b9c3d
Jakub Sałamun <Jakub.salamun98@gmail.com>
2021-04-11 19:56:13 +0200

    modyfikacja kalkulatora Bmi Bmr

--- FoodDiary/FoodDiary/Factories/BmiCalculator/IBmiCalculator.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace FoodDiary.Factories.BmiCalculator
-{
-    public interface IBmiCalculator
-    {
-        public double Calculate(double weight, int height, int age, double activityLevel);
-    }
-}
