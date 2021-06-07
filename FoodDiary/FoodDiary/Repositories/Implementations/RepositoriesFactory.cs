2cc335dad50e9ab730f3ee3644c3533e87d5acc6
Jakub Sałamun <Jakub.salamun98@gmail.com>
2021-06-07 17:38:31 +0200

    add_Target

--- FoodDiary/FoodDiary/Repositories/Implementations/RepositoriesFactory.cs
+++ FoodDiary/FoodDiary/Repositories/Implementations/RepositoriesFactory.cs
@@ -3,20 +3,27 @@ using System.Threading.Tasks;
 using FoodDiary.Data;
 using Repositories.Abstract;
 using FoodDiary.Repositories.Abstract;
+using Microsoft.AspNetCore.Identity;
+using FoodDiary.Models;
+using FoodDiary.Factories;
 
 namespace FoodDiary.Repositories.Implementations
 {
     public class RepositoriesFactory : IRepositoryFactory
     {
         private readonly ApplicationDbContext _context;
-        public RepositoriesFactory(ApplicationDbContext context)
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IBmiBmrFactory _bmibmrFactory;
+        public RepositoriesFactory(ApplicationDbContext context, UserManager<AppUser> userManager, IBmiBmrFactory bmibmrFactory)
         {
+            _userManager = userManager;
             _context = context;
+            _bmibmrFactory = bmibmrFactory;
         }
 
         public IUserRepository GetUserRepository()
         {
-            return new UserRepository(_context);
+            return new UserRepository(_context,_userManager,_bmibmrFactory);
         }
 
         public IProductsRepository GetProductsReposity()
